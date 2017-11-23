5e4fc4c97f34e486b0757429701a1eba59e66613
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2017-11-23 12:06:31 +0100

    groovy.

--- script.groovy
+++ script.groovy
@@ -1,4 +1,12 @@
 print 'Empieza el despliegue.'
-//./apic yes no
+
+def proc = "ls -ltr".execute()
+def b = new StringBuffer()
+proc.consumeProcessErrorStream(b)
+
+println proc.text
+println b.toString()
+
+//apic yes no
 //./apic login apim jose.delagala@ust-global.com !n0r1t5@C
 print 'Finaliza el despliegue.'
