5c0a68764821ecb11e647a39c392a6afb220e040
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2017-11-23 12:12:38 +0100

    groovy.

--- script.groovy
+++ script.groovy
@@ -1,18 +1,7 @@
 print 'Empieza el despliegue.'
 
-sh('apic -h')
+'apic -h'.execute()
 
 //apic yes no
 //./apic login apim jose.delagala@ust-global.com !n0r1t5@C
 print 'Finaliza el despliegue.'
-
-def sh(cmd) {
-    def proc = ["/bin/sh", "-c", cmd].execute()
-    proc.waitFor()
-    def exitValue = proc.exitValue()
-    if(exitValue != 0) {
-        System.err.println(stderrFuture.get())
-        throw new RuntimeException("$cmd returned $exitValue")
-    }
-    return stdoutFuture.get()
-}
