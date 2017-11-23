821455d8ce48dbf0efe9d08b82a0510b253239e6
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2017-11-23 12:08:41 +0100

    groovy.

--- script.groovy
+++ script.groovy
@@ -1,12 +1,21 @@
 print 'Empieza el despliegue.'
 
-def proc = "ls -ltr".execute()
-def b = new StringBuffer()
-proc.consumeProcessErrorStream(b)
-
-println proc.text
-println b.toString()
+sh('ls -ltr')
 
 //apic yes no
 //./apic login apim jose.delagala@ust-global.com !n0r1t5@C
 print 'Finaliza el despliegue.'
+
+def sh(cmd) {
+    def proc = ["/bin/sh", "-c", cmd].execute()
+    def pool = Executors.newFixedThreadPool(2)
+    def stdoutFuture = pool.submit({ -> proc.inputStream.text} as Callable<String>)
+    def stderrFuture = pool.submit({ -> proc.errorStream.text} as Callable<String>)
+    proc.waitFor()
+    def exitValue = proc.exitValue()
+    if(exitValue != 0) {
+        System.err.println(stderrFuture.get())
+        throw new RuntimeException("$cmd returned $exitValue")
+    }
+    return stdoutFuture.get()
+}
