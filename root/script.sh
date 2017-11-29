22f0a3a8a041054d104adfa99da2c84fe22f812f
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2017-11-29 19:07:56 +0100

    groovy.

--- root/script.sh
+++ root/script.sh
@@ -1,5 +1,9 @@
 echo "yes" | apic && echo "no" | apic
-echo "apim" | apic login && echo "jose.delagala@ust-global.com" | apic login && echo "!n0r1t5@C" | apic login
+#echo "apim" | apic login && echo "jose.delagala@ust-global.com" | apic login && echo "!n0r1t5@C" | apic login
+apic login << EOF
+"jose.delagala@ust-global.com"
+"!n0r1t5@C"
+EOF
 apic drafts:push deploy/formacionapirest_1.0.0.yaml --server apim --organization factoriaustglobal
 
 #apic login << EOF
