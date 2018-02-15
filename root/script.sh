569f63757f685d6545199395f1e85417a900ae4e
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2018-02-15 11:44:34 +0100

    sed. deploy

--- root/script.sh
+++ root/script.sh
@@ -2,7 +2,7 @@ echo "yes" | apic && echo "no" | apic
 
 sed -i 's/visibilidad/factoriaustglobal/g' deploy/prodplantilla.yaml
 
-apic publish prodplantilla.yaml --server apim --organization factoriaustglobal --catalog sb
+apic publish deploy/prodplantilla.yaml --server apim --organization factoriaustglobal --catalog sb
 #apic drafts:push deploy/formacionapirest_1.0.0.yaml --server apim --organization factoriaustglobal
 
 #apic login << EOF
