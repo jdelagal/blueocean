2b5b75ce0abbecc8acf9f811a5ba6d44f3621f72
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2018-02-21 17:06:38 +0100

    prueba deploy.

--- root/script.sh
+++ root/script.sh
@@ -3,9 +3,9 @@ echo "yes" | apic && echo "no" | apic
 
 echo "Visibilidad: $paramVisibilidad"
 
-sed -i 's/visibilidad/factoriaustglobal/g' deploy/prodplantilla.yaml
+sed -i 's/visibilidad/$paramVisibilidad/g' deploy/prodplantilla.yaml
 
-apic publish deploy/prodplantilla.yaml --server apim --organization factoriaustglobal --catalog sb
+apic publish deploy/prodplantilla.yaml --server apim --organization $paramVisibilidad --catalog sb
 #apic drafts:push deploy/formacionapirest_1.0.0.yaml --server apim --organization factoriaustglobal
 
 #apic login << EOF
