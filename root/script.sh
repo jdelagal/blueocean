fc7d6387dbd2d71cca32b02fe75642f71b715b1e
jdelagal <jdelagal@gmail.com>
2018-02-21 15:06:05 +0100

    Script con cambio de valor de parámetro

--- root/script.sh
+++ root/script.sh
@@ -1,8 +1,15 @@
 echo "yes" | apic && echo "no" | apic
 
-sed -i 's/visibilidad/factoriaustglobal/g' deploy/prodplantilla.yaml
+a='s/visibilidad/'
+a+=$1
+a+='/g'
 
-apic publish deploy/prodplantilla.yaml --server apim --organization factoriaustglobal --catalog sb
+sed -i $a deploy/prodplantilla.yaml
+
+apic publish deploy/prodplantilla.yaml --server apim --organization $1 --catalog sb
+
+#sed -i 's/visibilidad/factoriaustglobal/g' deploy/prodplantilla.yaml
+#apic publish deploy/prodplantilla.yaml --server apim --organization factoriaustglobal --catalog sb
 #apic drafts:push deploy/formacionapirest_1.0.0.yaml --server apim --organization factoriaustglobal
 
 #apic login << EOF
